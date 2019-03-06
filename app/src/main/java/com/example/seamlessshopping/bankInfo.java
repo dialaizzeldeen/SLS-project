e24350cb59a29a3ce9a10f67bfbf6f83c8d49d8b
dialaizzeldeen <dialadista@hotmail.com>
2019-03-06 22:53:55 +0200

    good version without error

--- app/src/main/java/com/example/seamlessshopping/bankInfo.java
+++ app/src/main/java/com/example/seamlessshopping/bankInfo.java
@@ -6,13 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class bankInfo extends AppCompatActivity {
-
+ EditText cardno,cardname,expdate,cvv;
+ Button submitbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_info);
+        cardno=findViewById(R.id.cardNo);
+        cardname=findViewById(R.id.cardName);
+        expdate=findViewById(R.id.expireDate);
+        cvv=findViewById(R.id.cvv);
+        submitbutton=findViewById(R.id.submit);
+
+
 
     }
 
