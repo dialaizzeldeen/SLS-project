cefa280b3a8995d4b55ead669bf9a25388777577
dialaizzeldeen <dialadista@hotmail.com>
2019-03-11 19:38:47 +0200

    new eddition

--- app/src/main/java/com/example/seamlessshopping/profile.java
+++ app/src/main/java/com/example/seamlessshopping/profile.java
@@ -7,16 +7,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.ReferenceQueue;
+
 
 public class profile extends AppCompatActivity {
+   RequestQueue rq;
+   String url=" 10.0.0.3";
     TextView username;
     Button save;
-    EditText  name,gender,location,bday,mobile,personalemail;
+    EditText  gender,location,bday,mobile,personalemail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        rq= Volley.newRequestQueue(this);
         username=findViewById(R.id.usernameID);
         gender=findViewById(R.id.gender);
         location=findViewById(R.id.location);
@@ -24,6 +39,42 @@ public class profile extends AppCompatActivity {
         mobile=findViewById(R.id.mobile);
         personalemail=findViewById(R.id.personalemail);
         save=findViewById(R.id.save);
+
+        sendjsonrequest();
+    }
+    public void sendjsonrequest (){
+        JsonObjectRequest JsonObjectRequest=new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                  String  usernamee=response.getString("username");
+                    String  Gender=response.getString("gender");
+                    String  Location=response.getString("location");
+                    String  Bday=response.getString("bday");
+                    String  Mobile=response.getString("mobile");
+                    String  Personalemail=response.getString("personalemail");
+
+                    username.setText(usernamee);
+                    gender.setText(Gender);
+                    location.setText(Location);
+                    bday.setText(Bday);
+                    mobile.setText(Mobile);
+                    personalemail.setText(Personalemail);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        rq.add(JsonObjectRequest);
     }
     public void saveP(View v){
         String usernamep=username.getText().toString();
