d221f207ce6ac850e115e09ac30b9564d20be05a
farahsmadi <nice_girl_farooha@hotmail.com>
2019-03-13 11:19:34 +0200

    heyyyy

--- app/src/main/java/com/example/seamlessshopping/profile.java
+++ app/src/main/java/com/example/seamlessshopping/profile.java
@@ -2,10 +2,12 @@ package com.example.seamlessshopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,6 +16,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,29 +24,96 @@ import java.lang.ref.ReferenceQueue;
 
 
 public class profile extends AppCompatActivity {
-   RequestQueue rq;
-   String url=" 10.0.0.3";
+    RequestQueue rq;
     TextView username;
     Button save;
-    EditText  gender,location,bday,mobile,personalemail;
+    EditText gender, location, bday, mobile, personalemail;
+    String url = "http://10.10.10.7/profilePage.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        rq= Volley.newRequestQueue(this);
-        username=findViewById(R.id.usernameID);
-        gender=findViewById(R.id.gender);
-        location=findViewById(R.id.location);
-        bday=findViewById(R.id.bday);
-        mobile=findViewById(R.id.mobile);
-        personalemail=findViewById(R.id.personalemail);
-        save=findViewById(R.id.save);
-
-        sendjsonrequest();
+        rq = Volley.newRequestQueue(this);
+        username = findViewById(R.id.usernameProfile);
+        gender = findViewById(R.id.genderP);
+        location = findViewById(R.id.locationP);
+        bday = findViewById(R.id.bdayP);
+        mobile = findViewById(R.id.mobileP);
+        personalemail = findViewById(R.id.personalemailP);
+        save = findViewById(R.id.saveP);
+        dataSaving(url);
+        //  sendjsonrequest();
     }
-    public void sendjsonrequest (){
-        JsonObjectRequest JsonObjectRequest=new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+    private void dataSaving(String url) {
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);  //
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        try {
+
+                            JSONArray responseArray = jsonObject.getJSONArray("profile");
+                            Log.i("Response", responseArray + "");
+                            Log.i("Response", jsonObject + "");
+                            //StringBuilder textViewData = new StringBuilder();
+                            //Parse the JSON response array by iterating over it
+                            // productsObjectArrayList.clear();
+
+                           /** for (int i = 0; i < responseArray.length(); i++) {
+                                JSONObject response = responseArray.getJSONObject(i);
+                                String usernamee = response.getString("username");
+                                String Gender = response.getString("gender");
+                                String Location = response.getString("location");
+                                String Bday = response.getString("bday");
+                                String Mobile = response.getString("mobile");
+                                String Personalemail = response.getString("personalemail");
+
+                                Log.d("response", "j" + response);
+
+
+                            }**/
+                            //   textView.setText(textViewData.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        //Display error message whenever an error occurs
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.e("Error", error.getMessage());
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        queue.add(jsObjRequest);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void sendjsonrequest () {}}
+       /* JsonObjectRequest JsonObjectRequest=new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
@@ -61,7 +131,7 @@ public class profile extends AppCompatActivity {
                     bday.setText(Bday);
                     mobile.setText(Mobile);
                     personalemail.setText(Personalemail);
-
+                    Log.d("response",response.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -74,19 +144,5 @@ public class profile extends AppCompatActivity {
 
             }
         });
-        rq.add(JsonObjectRequest);
-    }
-    public void saveP(View v){
-        String usernamep=username.getText().toString();
-        String genderp=gender.getText().toString();
-        String locationp=location.getText().toString();
-        String bdayp=bday.getText().toString();
-        String mobilep=mobile.getText().toString();
-        String personalemailp=personalemail.getText().toString();
-        String type ="save";
-
-        loginBackground loginBackground1= new loginBackground(this);
-        loginBackground1.execute(type,usernamep,genderp,locationp,bdayp,mobilep,personalemailp);
-    }
-}
+        rq.add(JsonObjectRequest);*/
 
