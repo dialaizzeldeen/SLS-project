b9bb5b611e8d5fa2e4b4ba57a314dd133176e12d
hudasalameh00 <hudasalameh97@gmail.com>
2019-03-17 02:36:58 +0200

    new eddition

--- app/src/main/java/com/example/seamlessshopping/profilecategory.java
+++ app/src/main/java/com/example/seamlessshopping/profilecategory.java
@@ -1,12 +1,11 @@
 package com.example.seamlessshopping;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class profilecategory extends AppCompatActivity {
 
@@ -14,29 +13,30 @@ public class profilecategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilecategory);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        TextView openProfile=findViewById(R.id.profileid);
+        TextView pymentPage=findViewById(R.id.paymentid);
+        TextView historyPage=findViewById(R.id.historyid);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        openProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(), profile.class);
+                startActivity(i);
+            }});
+        pymentPage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(),bankInfo.class);
+                startActivity(i);
+            }});
+        historyPage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),paymentHistory.class);
+                startActivity(i);
+            }});
+
+
+
+
     }
 
-    public void updateP (){
-            Intent i= new Intent(this,profile.class);
-            startActivity(i);
-    }
-  public void infoP(){
-      Intent i= new Intent(this,bankInfo.class);
-      startActivity(i);
-  }
-  public void historyP(){
-        Intent i=new Intent(this,paymentHistory.class);
-        startActivity(i);
-  }
+
 }
